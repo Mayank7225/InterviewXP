0ab219f1e4b7332c6e3374af4c706f1f3d7d5eb6
mayank singh <astronomyclub@vnit.ac.in>
2024-06-30 19:27:03 +0530

    added about section

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function Page(){
+    return <main className="p-5">
+        <section>
+            <div>
+                <h1>Authors</h1>
+            </div>
+        </section>
+    </main>
+}
