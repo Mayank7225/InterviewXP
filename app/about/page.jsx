a40135a16b80e1e81327c86d1949e98ea37ebf5d
mayank singh <astronomyclub@vnit.ac.in>
2024-07-01 07:23:31 +0530

    changes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,7 @@ export default function Page(){
     return <main className="p-5">
         <section>
             <div>
-                <h1>Authors</h1>
+                <h1>Authors Will be Uploaded Shortly</h1>
             </div>
         </section>
     </main>
