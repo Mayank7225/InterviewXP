ff3fa95acd70f3e98bf1b2f90e304ee2381e7aba
mayank singh <astronomyclub@vnit.ac.in>
2024-07-09 12:18:00 +0530

    changes

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -2,7 +2,7 @@ export default function Page(){
     return <main className="flex flex-col items-center justify-center p-4">
         <section>
         <h1 className="font-bold">Coming Soon ..............</h1>
-        <h1 className=""> Since placements are starting from 29 july will post regular updates.</h1>
+        <h1 className=""> Since placements are starting , will post regular updates.</h1>
         </section>
     </main>
 }
